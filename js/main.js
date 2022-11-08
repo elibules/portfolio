99be09161f1d28f5e8e744d0bdf220758c6e1e0e
elibules <elibules@gmail.com>
2022-11-08 13:35:07 -0500

    Changing page header on nav click

--- js/main.js
+++ js/main.js
@@ -9,16 +9,19 @@ window.addEventListener("hashchange", (e) => {
 function setNav() {
   let hash = window.location.hash.replace("#", "");
   $("a").css("color", "#015b7b");
+  $("a").removeClass("active-link");
   if (hash != "") {
-    console.log(hash);
     fetchContent(hash);
     $("#nav-" + hash).css("color", "#eaac0b");
+    $("#nav-" + hash).addClass("active-link");
   } else {
     fetchContent("work");
     $("#nav-work").css("color", "#eaac0b");
+    $("#nav-work").addClass("active-link");
   }
 }
 
 function fetchContent(page) {
+  $("#page-header").html(page);
   console.log(page);
 }
