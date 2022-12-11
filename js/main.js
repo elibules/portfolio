84747bb006ee462363627d0bb21e5aedaf370e3a
elibules <elibules@gmail.com>
2022-12-11 16:55:22 -0500

    Portfolio is finished up!

--- js/main.js
+++ js/main.js
@@ -22,10 +22,29 @@ function setNav() {
 }
 
 async function fetchContent(page) {
-  $("#page-content").hide();
   $("#page-header").html(page);
 
   $.get("content/" + page + ".html", (data) => {
+    $("#page-content").html(data).hide();
     $("#page-content").html(data).show(400);
+    if (page === "work") {
+      $.get("json/work.json", (data) => {
+        for (let i in data) {
+          $("#work-content").append(
+            `
+            <div class="work-item" id="work${i}">
+              <a style='background-image: url("img/${data[i].img}")' href="${data[i].link}" target="_blank">
+              </a>
+              <p class="work-title">${data[i].title}</p>
+              <p class="work-desc">${data[i].description}</p>
+            </div>
+          `
+          );
+
+          // $("#work" + i).hide();
+          // $("#work" + i).slideToggle(500 * i);
+        }
+      });
+    }
   });
 }
